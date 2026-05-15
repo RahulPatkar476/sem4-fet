c55595c81eec39bea659282bc48bfb4e819e2dd2
RahulPatkar476 <22359rahulpatkar@gmail.com>
2026-05-15 20:05:46 +0530

    Update twelve-script.js

--- Assignment/twelve-script.js
+++ Assignment/twelve-script.js
@@ -5,12 +5,8 @@ function startAuth() {
     const maxAttempts = 3;
 
     let messageElement = document.getElementById("access-message");
-
-    // DO...WHILE LOOP
     do {
         inputPin = prompt("Enter the 4-digit Secret PIN:");
-
-        // If Cancel button is pressed
         if (inputPin === null) {
             messageElement.innerHTML = "⚠ Authentication Cancelled.";
             messageElement.className = "denied";
@@ -19,22 +15,19 @@ function startAuth() {
 
         attempts++;
 
-        // Correct PIN check
         if (inputPin === SECRET_PIN) {
-            messageElement.innerHTML = "✅ ACCESS GRANTED. Welcome back!";
+            messageElement.innerHTML = "ACCESS GRANTED. Welcome back!";
             messageElement.className = "success";
             return;
         }
 
-        // Wrong PIN behavior
         alert(
-            "❌ Incorrect PIN!\nRemaining Attempts: " + 
+            "Incorrect PIN!\nRemaining Attempts: " + 
             (maxAttempts - attempts)
         );
 
     } while (attempts < maxAttempts);
 
-    // Failed after exhausting all attempts
-    messageElement.innerHTML = "❌ SYSTEM LOCKED. Too many failed attempts.";
+    messageElement.innerHTML = "SYSTEM IS LOCKED. Too many failed attempts.";
     messageElement.className = "denied";
 }
