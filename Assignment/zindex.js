357239b6b979e271f8e48c8c5deb62b20124e20f
RahulPatkar476 <22359rahulpatkar@gmail.com>
2026-05-27 14:43:19 +0530

    Update zindex.js

--- Assignment/zindex.js
+++ Assignment/zindex.js
@@ -1,13 +1,10 @@
 function showLayer(id) {
 
-    // Get all layers
     let layers = document.getElementsByClassName("layer");
 
-    // Remove active class from all layers
     for (let i = 0; i < layers.length; i++) {
         layers[i].classList.remove("active");
     }
 
-    // Add active class to selected layer
     document.getElementById(id).classList.add("active");
 }
